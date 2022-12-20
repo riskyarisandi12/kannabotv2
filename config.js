d4eb91bd5a40e817e682e46ad06718080f182fc0
riskyarisandi12 <88918919+riskyarisandi12@users.noreply.github.com>
2022-12-20 23:19:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -125,9 +125,9 @@ global.author = global.wm
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg' //Main Thumbnail
-global.thumb2 = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg'
-global.thumbbc = 'https://telegra.ph/file/d389f4acafac741f6592c.jpg' //For broadcast
+global.thumb = 'https://telegra.ph/file/56522ca965fdbcd631a70.jpg' //Main Thumbnail
+global.thumb2 = 'https://telegra.ph/file/a694239572e81e29d99d0.jpg'
+global.thumbbc = 'https://telegra.ph/file/10afd3639dc1883202c2c.jpg' //For broadcast
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
